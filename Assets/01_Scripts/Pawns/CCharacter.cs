94179e34619d14e7e5feaaf0923fab5663b86eb4
chunX2maru <netjin99@gmail.com>
2024-05-16 03:21:59 +0900

    기본 UI 에셋 추가 + 플레이어 피격

--- Assets/01_Scripts/Pawns/CCharacter.cs
+++ Assets/01_Scripts/Pawns/CCharacter.cs
@@ -1,50 +1,76 @@
+using System;
 using UnityEngine;
 
 namespace AlienProject
 {
-	[RequireComponent(typeof(CStaminaPoint))]
-	[AddComponentMenu("Alien Project/Character Component")]
-	public class CCharacter : PawnBase
-	{
-		private CStaminaPoint _sp;
+    [RequireComponent(typeof(CStaminaPoint))]
+    [AddComponentMenu("Alien Project/Character Component")]
+    public class CCharacter : PawnBase
+    {
+        private CStaminaPoint _sp;
 
-		// MARK: Inspector
+        // MARK: Inspector
 
-		#region IDamageable Impelementation
+        #region IDamageable Impelementation
 
-		public override void TakeDamage(float damageAmount)
-		{ }
+        /// <summary>
+        /// 플레이어에게 데미지를 입힙니다.<para></para>
+        /// 적의 무기측에서 호출 해야합니다.
+        /// </summary>
+        /// <param name="damageAmount"></param>
+        public override void TakeDamage(float damageAmount)
+        {
+            _hp.ApplyDamage(damageAmount);
+            CUIManager.UIManager.SetPlayerHP(_hp.Ratio);
+        }
 
-		#endregion // IDamageable Impelementation
+        /// <summary>
+        /// Test용 현재 적이 구현되어있지않아 테스트할수 없어서 만듬 - ssj
+        /// </summary>
+        [ContextMenu("TestPlayer_HIT")]
+        public void Test()
+        {
+            TakeDamage(10);
+            Debug.Log("TestPlayerHit" + _hp.Ratio);
+        }
 
-		// MARK: Inspector
+        #endregion // IDamageable Impelementation
 
-		[SerializeField]
-		private OCharacterData _characterData;
+        // MARK: Inspector
 
-		#region Unity Callbacks
+        [SerializeField] private OCharacterData _characterData;
 
-		protected override void Awake()
-		{
-			base.Awake();
+        #region Unity Callbacks
 
-			_sp = GetComponent<CStaminaPoint>();
-		}
+        protected override void Awake()
+        {
+            base.Awake();
 
-		protected override void Start()
-		{
-			base.Start();
+            _sp = GetComponent<CStaminaPoint>();
+        }
 
-			if (_shouldInitalizeWithPawnData)
-			{
-				foreach (var initializable in GetComponents<IInitializable>())
-				{
-					initializable.Initialize(_characterData);
-				}
-			}
-		}
+        protected override void Start()
+        {
+            base.Start();
 
-		#endregion // Unity Callbacks
+            if (_shouldInitalizeWithPawnData)
+            {
+                foreach (var initializable in GetComponents<IInitializable>())
+                {
+                    initializable.Initialize(_characterData);
+                }
+            }
+        }
 
-	} // class CCharacter
+        public void OnTriggerEnter(Collider other)
+        {
+            //other.tag
+        }
+
+        public void OnCollisionEnter(Collision other)
+        {
+        }
+
+        #endregion // Unity Callbacks
+    } // class CCharacter
 } // namespace AlienProject
